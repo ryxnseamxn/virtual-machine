99a25df16ab306dd341d6bbb8edaaf5fe7a96073
Ryan Seaman <ryan.l.seaman@gmail.com>
2024-12-19 19:29:08 -0600

    added structure for main loop and a logic for a few instructions

--- src/main.c
+++ src/main.c
@@ -13,6 +13,10 @@
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];  
 
+uint16_t sign_extend(uint16_t x, int bit_count);
+
+void update_flags(uint16_t r); 
+
 enum
 {
     R_R0 = 0,
@@ -57,3 +61,164 @@ enum
     OP_TRAP    /* execute trap */
 };
 
+int main(int argc, const char* argv[])
+{
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+    // @{Setup}
+
+    reg[R_COND] = FL_ZRO;
+
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                {
+                    /* destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    /* first operand (SR1) */
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    /* whether we are in immediate mode */
+                    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                    if (imm_flag)
+                    {
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] + imm5;
+                    }
+                    else
+                    {
+                        uint16_t r2 = instr & 0x7;
+                        reg[r0] = reg[r1] + reg[r2];
+                    }
+
+                    update_flags(r0);
+                }
+                break;
+            case OP_AND:
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7; 
+                    uint16_t r1 = (instr >> 6) & 0x7; 
+                    uint16_t imm_flag = (instr >> 5) & 0x1; 
+
+                    if(imm_flag){
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] & imm5; 
+                    }else{
+                        uint16_t r2 = (instr >> 3) & 0x7; 
+                        reg[r0] = reg[r1] & reg[r2]; 
+                    }
+
+                    update_flags(r0); 
+                }
+                break;
+            case OP_NOT:
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t r1 = (instr >> 6) & 0x7; 
+
+                    reg[r0] = ~reg[r1]; 
+                    update_flags(r0); 
+                }
+                break;
+            case OP_BR:
+                // @{BR}
+                break;
+            case OP_JMP:
+                {
+                    uint16_t r0 = (instr >> 6) & 0x7; 
+                    reg[R_PC] = reg[r0]; 
+                }
+                break;
+            case OP_JSR:
+                // @{JSR}
+                break;
+            case OP_LD:
+                // @{LD}
+                break;
+            case OP_LDI:
+                {
+                    /* destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    /* PCoffset 9*/
+                    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+                    /* add pc_offset to the current PC, look at that memory location to get the final address */
+                    reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+                    update_flags(r0);
+                }
+                break;
+            case OP_LDR:
+                // @{LDR}
+                break;
+            case OP_LEA:
+                // @{LEA}
+                break;
+            case OP_ST:
+                // @{ST}
+                break;
+            case OP_STI:
+                // @{STI}
+                break;
+            case OP_STR:
+                // @{STR}
+                break;
+            case OP_TRAP:
+                // @{TRAP}
+                break;
+            case OP_RES:
+                break; 
+            case OP_RTI:
+                break; 
+            default:
+                // @{BAD OPCODE}
+                break;
+        }
+    }
+    // @{Shutdown}
+}
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
